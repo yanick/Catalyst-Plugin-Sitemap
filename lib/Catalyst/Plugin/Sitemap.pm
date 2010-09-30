38ac0ad20634e83830c186b2070f6bfde24e0ed0
Yanick Champoux <yanick@babyl.dyndns.org>
2010-09-29 20:16:33 -0400

    preparing for a first release

--- lib/Catalyst/Plugin/Sitemap.pm
+++ lib/Catalyst/Plugin/Sitemap.pm
@@ -1,4 +1,137 @@
 package Catalyst::Plugin::Sitemap;
+# ABSTRACT: Sitemap support for Catalyst.
+
+=head1 SYNOPSIS
+
+    # in MyApp.pm
+
+    use Catalyst qw/ Sitemap /;
+
+    # in the controller
+    
+    sub alone :Local :Sitemap { 
+        ... 
+    }
+    
+    sub with_priority :Local :Sitemap(0.75) {
+        ... 
+    }
+    
+    sub with_args :Local
+            :Sitemap( lastmod => 2010-09-27, changefreq => daily ) {
+        ...
+    }
+    
+    sub with_function :Local :Sitemap(*) { 
+        ... 
+    }
+    
+    sub with_function_sitemap {
+        $_[2]->add( 'http://localhost/with_function' );
+    }
+
+    # and then...
+    
+    sub sitemap : Path('/sitemap') {
+        my ( $self, $c ) = @_;
+ 
+        $c->res->body( $c->sitemap->xml );
+    }
+
+=head1 DESCRIPTION
+
+L<Catalyst::Plugin::Sitemap> provides a way to semi-automate the creation 
+of the sitemap of a Catalyst application.
+
+=head1 CONTEXT METHOD
+
+=head2 sitemap()
+
+Returns a L<Search::Sitemap> object. The sitemap object is populated by 
+inspecting the controllers of the application for actions with the 
+sub attribute C<:Sitemap>.
+
+=head1 C<:Sitemap> Subroutine Attribute
+
+The sitemap is populated by actions ear-marked with the <:Sitemap> sub
+attribute.  It can be invoked in different ways:
+
+=over
+
+=item C<:Sitemap>
+
+    sub alone :Local :Sitemap {
+        ...
+    }
+
+Adds the url of the action to the sitemap.  
+
+If the action does not
+resolves in a single url, this will results in an error.
+
+=item C<:Sitemap($priority)>
+
+
+    sub with_priority :Local :Sitemap(0.9) {
+        ...
+    }
+
+Adds the url, with the given number, which has to be between 1 (inclusive)
+and 0 (exclusive), as its priority.
+
+If the action does not
+resolves in a single url, this will results in an error.
+
+=item C<:Sitemap( %attributes )>
+
+    sub with_args :Local
+            :Sitemap( lastmod => 2010-09-27, changefreq => daily ) {
+        ...
+    }
+
+Adds the url with the given entry attributes (as defined by C<Search::Sitemap>).
+
+If the action does not
+resolves in a single url, this will results in an error.
+
+=item C<:Sitemap(*)>
+
+    sub with_function :Local :Sitemap(*) { }
+    
+    sub with_function_sitemap {
+        my ( $self, $c, $sitemap ) = @_;
+
+        $sitemap->add( 'http://localhost/with_function' );
+    }
+
+Calls the function 'I<action>_sitemap', if it exists, and passes it the
+controller, context and sitemap objects.
+
+This is currently the only way to invoke C<:Sitemap> on an action 
+resolving to many urls. 
+
+=back
+
+
+=head1 BUGS AND LIMITATIONS
+
+Currently, each invocation of the method C<sitemap()> will 
+re-generate the C<Search::Sitemap> object.  A future version
+of this module might offer a way to only compute the sitemap
+once.
+
+
+=head1 SEE ALSO
+
+=over 
+
+=item L<Search::Sitemap>
+
+=item http://babyl.dyndns.org/techblog/entry/catalyst-plugin-sitemap
+
+=back
+
+=cut
 
 use strict;
 use warnings;
@@ -67,23 +200,3 @@ sub sitemap {
 1;
 
 __END__
-
-ways I can do it
-
-is Sitemap( <yadah> ) ? => use it as is
-else => has <action>_sitemap? => use it
-else => use the uri directly
-
-
-sitemap => sub {
-    my ( $self, $c, $sitemap ) = @_;
-
-    ...
-};
-
-sub action_sitemap {
-    my ( $self, $c, $sitemap ) = @_;
-}
-
-sub do_stuff :Local :Sitemap {
-}
