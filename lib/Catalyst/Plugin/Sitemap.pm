edb6d83aef6bcb85beb99272f10949be0005635d
Yanick Champoux <yanick@babyl.dyndns.org>
2015-02-22 21:25:09 -0500

    refactor

--- lib/Catalyst/Plugin/Sitemap.pm
+++ lib/Catalyst/Plugin/Sitemap.pm
@@ -27,52 +27,46 @@ sub _build_sitemap {
     my $sitemap = WWW::Sitemap::XML->new;
 
     for my $controller ( map { $self->controller($_) } $self->controllers ) {
-      ACTION:
-        for my $a ( $controller->get_action_methods ) {
-
-            my $action = $controller->action_for( $a->name );
-
-            my $attr = $action->attributes->{Sitemap} or next ACTION;
-
-            die "more than one attribute 'Sitemap' for sub ",
-              $a->fully_qualified_name
-              if @$attr > 1;
-
-            my @attr = split /\s*(?:,|=>)\s*/, $attr->[0];
-
-            my %uri_params;
-
-            if ( @attr == 1 ) {
-                if ( $attr[0] eq '*' ) {
-                    my $sitemap_method = $action->name . "_sitemap";
-
-                    if ( $controller->can($sitemap_method) ) {
-                        $controller->$sitemap_method( $self, $sitemap );
-                        next ACTION;
-                    }
-                }
-
-                if ( $attr[0] + 0 > 0 ) {
-
-                    # it's a number
-                    $uri_params{priority} = $attr[0];
-                }
-
-            }
-            elsif ( @attr > 0 ) {
-                %uri_params = @attr;
-            }
-
-            $uri_params{loc} = $self->uri_for_action( $action->private_path );
-
-            $sitemap->add(%uri_params);
+        for my $action ( $controller->get_action_methods ) {
+            $self->_add_controller_action_to_sitemap( $sitemap, $controller, $action );
         }
-
     }
 
     return $sitemap;
 }
 
-1;
+sub _add_controller_action_to_sitemap {
+    my( $self, $sitemap, $controller, $act ) = @_;
 
-__END__
+    my $action = $controller->action_for($act->name);
+
+    my $attr = $action->attributes->{Sitemap} or return;
+
+    die "more than one attribute 'Sitemap' for sub ", $act->fully_qualified_name
+        if @$attr > 1;
+
+    my @attr = split /\s*(?:,|=>)\s*/, $attr->[0];
+
+    my %uri_params;
+
+    if ( @attr == 1 ) {
+        if ( $attr[0] eq '*' ) {
+            my $sitemap_method = $action->name . "_sitemap";
+
+            return $controller->$sitemap_method( $self, $sitemap )
+                if  $controller->can($sitemap_method);
+        }
+        elsif ( $attr[0] + 0 > 0 ) { # it's a number
+            $uri_params{priority} = $attr[0];
+        }
+    }
+    elsif ( @attr > 0 ) {
+        %uri_params = @attr;
+    }
+
+    $uri_params{loc} = $self->uri_for_action( $action->private_path );
+
+    $sitemap->add(%uri_params);
+}
+
+1;
